e1d6092d3c065456b1792c2e0718a5259e6a1e23
Manuel Marcen <manuel.marcen@gmail.com>
2024-12-25 17:42:56 +0100

    .

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hola")
+from saludos import hola, adios
+
+print(adios())
+name = input("¿Cuál es tu nombre? ")
+apellido = input("¿Cuál es tu apellido? ")
+print(hola(name, apellido))
+
---
+++ saludos.py
@@ -0,0 +1,6 @@
+
+def hola(nombre, apellido):
+    return f"Hola, {nombre} {apellido}"
+
+def adios():
+    return "adios"
