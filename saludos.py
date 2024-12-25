6f0d444c9ebf84d4259c3a482187cfd3d0da1f29
Manuel Marcen <manuel.marcen@gmail.com>
2024-12-25 17:55:46 +0100

    .

--- saludos.py
+++ saludos.py
@@ -1,6 +1,21 @@
-
 def hola(nombre, apellido):
+    """
+    Devuelve un saludo personalizado.
+
+    Args:
+        nombre (str): El nombre de la persona.
+        apellido (str): El apellido de la persona.
+
+    Returns:
+        str: Un saludo personalizado.
+    """
     return f"Hola, {nombre} {apellido}"
 
 def adios():
+    """
+    Devuelve una despedida.
+
+    Returns:
+        str: Una despedida.
+    """
     return "adios"
---
+++ tests/test_main.py
@@ -0,0 +1,4 @@
+from main import * 
+
+def test_hello_world():
+    assert hello_world() == "Hello, World!"
